943365d3cd995fd8d5152ec460133ac3f7014507
Rajat Kukde <rajatku@cybage.com>
2020-10-09 18:28:31 +0530

    9th oct workshop RWD

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/*function of navigation bar*/
+function showhidemenu(){
+    let menu = document.querySelector("#nav-bar")
+    if(menu.style.display==="flex"){
+        menu.style.display = 'none'
+    }else{
+        menu.style.display = 'flex'
+    }
+}
+
+
